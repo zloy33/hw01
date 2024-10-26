af51de6e662f92290e7bd3821d40c98b8d2ead77
artem <kalach563@gmail.com>
2024-10-26 12:32:35 +0300

    Add files via upload

---
+++ home work.py
@@ -0,0 +1,14 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99 > 9.98)and(1000!=1000.1)
+# 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+# 4th program
+print('123,456')
+print (int(float('123.456')))
+print (int(float('123.456')*10))
+print (int(float('123.456'))%10)
+print(int(float('123.456') * 10) % 10) # не пойму как работает логика чтобы получилась 4
